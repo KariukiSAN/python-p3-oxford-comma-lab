4b39d3cf89df9a93567c2490bff9b39baadbd008
KariukiSAN <kariukian1@gmail.com>
2023-10-14 15:41:53 +0300

    R1

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,9 @@
 def oxford_comma(items):
-    return None
+    def oxford_comma(items):
+        if lens(items) == 1:
+            return items [0]
+        elif len (items)==2:
+            return f"{items[0]} and {items[1]}"
+        elif len(items) >= 3:
+            formatted_items = ", ".join(items[:-1])
+            return f"{formatted_items}, and {items[-1]}"
